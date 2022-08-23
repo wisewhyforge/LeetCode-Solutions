14f7ab2f80759a1595945a1824647a29b57b1ebb
JLuo0 <duitNN@gmail.com>
2022-08-23 15:33:20 -0400

    Added Palindrome Linked List O(n) average runtime and memory Solution

---
+++ Palindrome Linked List/Solution1.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        ListNode temp = head;
+        while (temp != null) {
+            list.add(temp.val);
+            temp = temp.next;
+        }
+        
+        for (int i = 0; i < list.size() / 2; i++) {
+            if (list.get(i) != list.get(list.size() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
